eb61c73d24b06d45002befc45e4219f5ae340fe4
Masen Furer <masenf@gmail.com>
2013-01-10 11:26:44 -0700

    extra check on git command

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 cd ~/masenf-com
-git pull origin master
-if make ; then
-    echo Copying data to webroot
-    cp -a output/* ~/public_html/masenf.com/
+if git pull origin master ; then
+    if make ; then
+        echo Copying data to webroot
+        cp -a output/* ~/public_html/masenf.com/
+    else
+        echo Build failed, nothing deployed
+    fi
 else
-    echo Build failed, nothing deployed
+    echo Couldn\'t update from origin, nothing deployed
 fi
