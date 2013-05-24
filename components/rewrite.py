93d2be1919cac26bb58999ad5e479192d5502847
masenf <masenf@gmail.com>
2013-05-24 02:18:35 -0700

    add rudimentary flags support for rewrite component

--- components/rewrite.py
+++ components/rewrite.py
@@ -6,11 +6,11 @@ class Rewrite(Component):
         Component.__init__(self, *args, **kwds)
 
         # add predefined rewrites
-        self.rewrites = { '/?' : '/home.html',
-                            'blog/?' : '/blog.html',
-                            'blog/tag/([\ a-zA-Z]+)' : '/blog/tag/$1.html',
-                            'blog/archive/([0-9]{4})-([0-9]{2})/?' : '/blog/archive/$1-$2.html',
-                            'blog/rss2/?' : '/feed.rss' }
+        self.rewrites = { '/?' : ('/home.html',''),
+                            'blog/?' : ('/blog.html','[R]'),
+                            'blog/tag/([\ a-zA-Z]+)' : ('/blog/tag/$1.html','[R]'),
+                            'blog/archive/([0-9]{4})-([0-9]{2})/?' : ('/blog/archive/$1-$2.html','[R]'),
+                            'blog/rss2/?' : ('/feed.rss','[R]') }
         self.index_permalinks()
         self.write_htaccess()
     def index_permalinks(self):
@@ -20,12 +20,12 @@ class Rewrite(Component):
                 if "rewrite" in c.metadata:
                     pl = c.metadata["rewrite"].split(" ")
                     for p in pl:
-                        self.rewrites[p] = c.metadata["link"]
+                        self.rewrites[p] = (c.metadata["link"],'[R]')
     def write_htaccess(self, filename="output/.htaccess"):
         self.log("Rewrite","Writing Rewrite Rules to {}...".format(filename),self.data.level)
         with open(filename, 'w') as f:
             f.write("RewriteEngine on\n")
             for pl, l in self.rewrites.iteritems():
-                f.write("RewriteRule ^{}$ {} [R]\n".format(pl,l))
+                f.write("RewriteRule ^{}$ {} {}\n".format(pl,*l))
 
 
